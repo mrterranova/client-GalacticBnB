7fc6fc6878bf0f337a489a3fe83fd1a946a2aa42
avalojc <55007242+avalojc@users.noreply.github.com>
2020-12-02 09:34:11 -0500

    Feature/click n state (#55)
    * updated click actions
    
    * CSS work, state, active, hover
    
    * update compnnt

--- src/app/cancel-flex/cancel-flex.component.ts
+++ src/app/cancel-flex/cancel-flex.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CancelFlexComponent implements OnInit {
   
+  show: boolean = false;
+  select: boolean = false;
+
   constructor() { }
+
+  displayToggle():void{
+    this.show = !this.show;
+    console.log(this.show)
+  }
+
+  checkToggle():void{
+    this.select = !this.select;
+    document.getElementById("tdrindiv").classList.add("activeCheck0");
+    document.getElementById("tdrbutton").classList.add("activeCheck1");
+    document.getElementById("tdrindiv").classList.add("activeCheck2");
+    console.log(this.select)
+  }
 
   ngOnInit(): void {
   }
