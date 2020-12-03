e1326e1f15af7c25f9b84d039b8365eeea558ef7
BLoveless <bloveless@talentpath.com>
2020-12-03 12:55:29 -0500

    front page search input works and exists

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -19,15 +19,58 @@ export class SearchBarComponent implements OnInit {
 
   ngOnInit(): void {
     (function ($) {
-      $(document).ready(function () {
-        var apigatewayendpoint =
-          'https://search-galacticlistings-t5gsylsxiuyovdyaa7vwlwldce.us-east-1.es.amazonaws.com/serverdata/_search/';
+      $(document).ready(function(){
+        var apigatewayendpoint = 'https://search-galacticlistings-t5gsylsxiuyovdyaa7vwlwldce.us-east-1.es.amazonaws.com/serverdata/_search/';
 
-         const loadingdiv = $('#loading');
-         const noresults = $('#noresults');
-         const resultdiv = $('#results');
+var loadingdiv = $('#loading');
+var noresults = $('#noresults');
+var resultdiv = $('#results');
+var searchbox = $('input#search');
+var timer = 0;
+
+// Executes the search function 250 milliseconds after user stops typing
+searchbox.keyup(function () {
+  clearTimeout(timer);
+  timer = setTimeout(search, 250);
+});
+
+async function search() {
+  // Clear results before searching
+  noresults.hide();
+  resultdiv.empty();
+  loadingdiv.show();
+  // Get the query from the user
+  let query = searchbox.val();
+  // Only run a query if the string contains at least two characters
+  if (query.length > 2) {
+    // Make the HTTP request with the query as a parameter and wait for the JSON results
+    let response = await $.get(apigatewayendpoint, {
+      q: `${query}*`,
+      size: 25
+    }, 'json');
+    // Get the part of the JSON response that we care about
+    let results = response['hits']['hits'];
+    if (results.length > 0) {
+      loadingdiv.hide();
+      // Iterate through the results and write them to HTML
+      resultdiv.append('<p>Found ' + results.length + ' results.</p>');
+      console.log(results)
+  
+    } else {
+      noresults.show();
+    }
+  }
+  loadingdiv.hide();
+}
+
+// Tiny function to catch images that fail to load and replace them
+function imageError(image) {
+  image.src = 'images/no-image.png';
+}
       });
-    });
+    })(jQuery);
+
+
   }
 
   receiveAdults($event) {
@@ -50,36 +93,9 @@ export class SearchBarComponent implements OnInit {
 
   test() {
     console.log(this.value + '*');
+    this.ngOnInit();
   }
 
 
-  async search() {
-  //   // Clear results before searching
-  //   noresults.hide();
-  //   resultdiv.empty();
-  //   loadingdiv.show();
-  //   // Get the query from the user
-  //   let query = this.value;
-  //   // Only run a query if the string contains at least two characters
-  //   if (query.length > 2) {
-  //     // Make the HTTP request with the query as a parameter and wait for the JSON results
-  //     let response = await $.get(apigatewayendpoint, {
-  //       q: `${query}*`,
-  //       size: 25
-  //     }, 'json');
-  //     // Get the part of the JSON response that we care about
-  //     let results = response['hits']['hits'];
-  //     if (results.length > 0) {
-  //       loadingdiv.hide();
-  //       // Iterate through the results and write them to HTML
-  //       resultdiv.append('<p>Found ' + results.length + ' results.</p>');
-  //       console.log(results)
-    
-  //     } else {
-  //       noresults.show();
-  //     }
-  //   }
-  //   loadingdiv.hide();
-   }
 
 }
